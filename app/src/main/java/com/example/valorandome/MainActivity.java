220b11bf69f1f0aea6f2ff410f427d250d57c023
Brackman12 <alejandro.camacho@alumnos.udg.mx>
2022-04-18 22:10:58 -0500

    Primer Commit!

---
+++ app/src/main/java/com/example/valorandome/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.valorandome;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.view.*;
+import android.util.Log;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      //  ConstraintLayout myLayout = (ConstraintLayout) findViewById(R.id.textView5);
+        //myLayout.setOnTouchListener(new View.OnTouchListener(){
+          //  @Override
+            //    public boolean onTouch(View v, MotionEvent event){
+              //  Log.i("TouchEvents","Touch is detected");
+
+              //  return true;
+
+           // }});
+
+    }
+}
