fc2fb86db0b4dbb388a28770f02369f298f5e438
Brackman12 <alejandro.camacho@alumnos.udg.mx>
2022-04-22 18:21:37 -0500

    Cuarto Commit, No hay errores, se puede compilar sin error. ¡Ya funciona el menu de navegacion inferior! Las interfaces tienen diseños basicos para identificar los layouts pero ya se puede navegar

--- app/src/main/java/com/example/valorandome/Menu_Principal.java
+++ app/src/main/java/com/example/valorandome/Menu_Principal.java
@@ -2,21 +2,53 @@ package com.example.valorandome;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.valorandome.databinding.ActivityMainBinding;
+import com.example.valorandome.databinding.ActivityMenuPrincipalBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Menu_Principal extends AppCompatActivity {
 
+    ActivityMenuPrincipalBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_principal);
+        //setContentView(R.layout.activity_menu_principal);
+        binding = ActivityMenuPrincipalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
+        binding.bottomNavigationView.setOnItemSelectedListener( item -> {
 
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+            return true;
+        });
     }
 
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_layout,fragment);
+        fragmentTransaction.commit();
+
+    }
 }
