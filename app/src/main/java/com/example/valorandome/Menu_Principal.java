a0aa8175e2fa7f6966fe26e6dbbe741ddfa18b56
Brackman12 <alejandro.camacho@alumnos.udg.mx>
2022-04-23 15:44:04 -0500

    Sexto Commit, No hay errores, se puede compilar sin error. Tiene los 9 Contenedores de las 9 semanas, solo la semana 8 y 9 tienen la funcion onClick, los colores no se han cambiado

--- app/src/main/java/com/example/valorandome/Menu_Principal.java
+++ app/src/main/java/com/example/valorandome/Menu_Principal.java
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -54,4 +56,8 @@ public class Menu_Principal extends AppCompatActivity {
 
     }
 
+    public void SiguienteDos(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
